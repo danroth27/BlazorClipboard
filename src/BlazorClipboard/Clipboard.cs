e247681eef8c1da4548622adea630e6d7e3281f2
Daniel Roth <daroth@microsoft.com>
2018-06-07 01:29:52 -0700

    Add clipboard API

---
+++ src/BlazorClipboard/Clipboard.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Blazor.Browser.Interop;
+
+namespace BlazorClipboard
+{
+    public class Clipboard
+    {
+        public static Task<object> WriteText(string text)
+        {
+            return RegisteredFunction.InvokeAsync<object>("BlazorClipboard.WriteText", text);
+        }
+    }
+}
---
+++ src/BlazorClipboard/content/clipboard.js
@@ -0,0 +1,7 @@
+﻿// This file is to show how a library package may provide JavaScript interop features
+// wrapped in a .NET API
+
+Blazor.registerFunction('BlazorClipboard.WriteText', function (text) {
+    return navigator.clipboard.writeText(text);
+});
+    
